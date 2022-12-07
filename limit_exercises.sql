1db1de3843eebca5453f02c82e2b4dda2395c1a4
Maikel Karam <maikel.m.karam@gmail.com>
2022-12-07 15:25:39 -0600

    Limit exercise completed

---
+++ limit_exercises.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT last_name FROM employees.employees ORDER BY last_name desc limit 10;
+
+SELECT emp_no FROM employees.salaries order by salary DESC LIMIT 5;
+
+SELECT emp_no FROM employees.salaries order by salary DESC LIMIT 5 OFFSET 50;
+
