4ce48833c20275a923b84aef5a8ba3789b22b831
Maikel Karam <maikel.m.karam@gmail.com>
2022-12-08 14:31:18 -0600

    Group BY exercises progress save

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,3 +1,6 @@
 SELECT DISTINCT title FROM employees.titles;
 
-SELECT * FROM employees.employees WHERE last_name LIKE "E%" OR last_name LIKE "%e" ORDER BY emp_no desc;
+SELECT first_name, last_name FROM employees.employees WHERE last_name LIKE "E%" AND last_name LIKE "%e" GROUP BY first_name, last_name;
+
+SELECT last_name FROM employees.employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
+
