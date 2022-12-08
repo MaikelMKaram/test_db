d5c61c03be7520be0b68b833d765841df7e5b766
Maikel Karam <maikel.m.karam@gmail.com>
2022-12-08 14:33:39 -0600

    Group BY exercises progress save

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -4,3 +4,7 @@ SELECT first_name, last_name FROM employees.employees WHERE last_name LIKE "E%" 
 
 SELECT last_name FROM employees.employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
+SELECT DISTINCT COUNT(last_name), last_name FROM employees.employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
+
+SELECT DISTINCT COUNT(*), gender FROM employees.employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') GROUP BY gender;
+
