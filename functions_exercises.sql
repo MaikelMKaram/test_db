b0055de8b524201686be1abe7c34f31f512cb121
Maikel Karam <maikel.m.karam@gmail.com>
2022-12-08 09:47:36 -0600

    Functions exercise completed

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,3 +1,9 @@
 SELECT CONCAT(first_name, " ", last_name) FROM employees.employees WHERE (first_name = "Irena" OR first_name = "Vidya" OR first_name = "Maya") AND gender = "M" ORDER BY last_name, first_name;
 
 SELECT * FROM employees.employees WHERE month(birth_date) = 12 AND day(birth_date) = 25;
+
+SELECT * FROM employees.employees WHERE month(birth_date) = 12 AND day(birth_date) = 25 AND year(hire_date) BETWEEN 1990 AND 1999 ORDER BY hire_date DESC;
+
+SELECT datediff(curdate(), hire_date) FROM employees.employees WHERE month(birth_date) = 12 AND day(birth_date) = 25 AND year(hire_date) BETWEEN 1990 AND 1999 ORDER BY hire_date DESC ;
+
+
